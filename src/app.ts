2bdf37c7432191700f9bd3403a73e05e9c3577cb
Max <122755287+theclouday@users.noreply.github.com>
2024-06-10 01:51:19 +0200

    init

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import mongoose, { ConnectOptions } from 'mongoose';
 export default async () => {
     const app = express();
     const port = 3000;
-    const mongoAdress = 'mongodb+srv://admin:MDuNgc3phzMIfFvv@h-a4.o34bkq1.mongodb.net/?retryWrites=true&w=majority&appName=h-a4';
+    const mongoAdress = 'mongodb+srv://admin:**some adress**';
 
     app.use('/', routers);
 
